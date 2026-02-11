cd27fe4f54440d60bcfc9efaa6c92e50c4586bd4
wanjohi lewi <wanjohilewi7@gmail.com>
2026-02-11 19:29:25 +0300

    another approach for quicksort

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -2,12 +2,22 @@ package sorting;
 
 public class QuickSort {
 
-    public static int partition(int[] arr, int low, int high) {
+    public static void quickSort(int[] arr, int low, int high){
+        if(low<high){
 
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi -1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
+        int i = low -1;
+
+        for(int j= low; j<high; j++){
+            if(arr[j] < pivot){
                 i++;
 
                 int temp = arr[i];
@@ -15,37 +25,68 @@ public class QuickSort {
                 arr[j] = temp;
             }
         }
-
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
         arr[high] = temp;
 
-        return i + 1;
-    }
+        return i+1;
 
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-
-            int pi = partition(arr, low, high);
-
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
     }
 
     public static void main(String[] args) {
-
-        int[] arr = {8, 3, 5, 2, 9, 1};
-
-        System.out.println("Before sorting:");
-        for (int num : arr)
-            System.out.print(num + " ");
+        int arr[] = {5, 62, 2, 3, 111, 81, 4};
 
         quickSort(arr, 0, arr.length - 1);
 
-        System.out.println("\nAfter sorting:");
-        for (int num : arr)
+        for(int num : arr){
             System.out.print(num + " ");
+        }
     }
+
+//    public static int partition(int[] arr, int low, int high) {
+//
+//        int pivot = arr[high];
+//        int i = low - 1;
+//        for (int j = low; j < high; j++) {
+//            if (arr[j] < pivot) {
+//                i++;
+//
+//                int temp = arr[i];
+//                arr[i] = arr[j];
+//                arr[j] = temp;
+//            }
+//        }
+//
+//        int temp = arr[i + 1];
+//        arr[i + 1] = arr[high];
+//        arr[high] = temp;
+//
+//        return i + 1;
+//    }
+//
+//    public static void quickSort(int[] arr, int low, int high) {
+//        if (low < high) {
+//
+//            int pi = partition(arr, low, high);
+//
+//            quickSort(arr, low, pi - 1);
+//            quickSort(arr, pi + 1, high);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int[] arr = {8, 3, 5, 2, 9, 1};
+//
+//        System.out.println("Before sorting:");
+//        for (int num : arr)
+//            System.out.print(num + " ");
+//
+//        quickSort(arr, 0, arr.length - 1);
+//
+//        System.out.println("\nAfter sorting:");
+//        for (int num : arr)
+//            System.out.print(num + " ");
+//    }
 }
 
